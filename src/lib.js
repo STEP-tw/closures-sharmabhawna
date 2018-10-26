23fdc358e0f5a7481cb7514be4bf277e27cd45c2
Bhawna Sharma <shbhawna@In-shbhawna.local>
2018-10-26 10:50:00 +0530

    Modified makeFiboGenerator function handles all tests

--- src/lib.js
+++ src/lib.js
@@ -52,7 +52,19 @@ const makeCycler = function(array){
   }
 }
 
-const makeFiboGenerator = undefined;
+const makeFiboGenerator = function(arg1, arg2){
+  let firstNum = -arg1;
+  let secondNum = arg2-1;
+  if(arg2 == undefined){ firstNum = -arg1; secondNum = arg1; }
+  if(arg1 == undefined && arg2 == undefined){ secondNum = 1; firstNum = -1; }
+  return function(){
+    let nextNum = firstNum + secondNum;
+    firstNum = secondNum;
+    secondNum = nextNum;
+    return nextNum;
+  }
+}
+
 const curry = function(func, argument){
   return function(parameter1, parameter2){
     return func(argument, parameter1, parameter2);
